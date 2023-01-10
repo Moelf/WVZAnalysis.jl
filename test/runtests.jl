4908886af8f70f5a4a131f0aad7273baf9a6985e
Moelf <proton@jling.dev>
2023-01-09 22:54:37 -0600

    add some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,13 @@
 using WVZAnalysis
+using WVZAnalysis.FHist
 using Test
 
-@testset "WVZAnalysis.jl" begin
-    # Write your tests here.
-    fs = prep_tasks("Signal"; shape_variation="NOMINAL", sfsys=false);
-    @assert length(fs) == 33
-    res = main_looper(fs[1])
-
+if endswith(gethostname(), "uchicago.edu")
+    @testset "WVZAnalysis.jl" begin
+        fs = prep_tasks("Signal"; shape_variation="NOMINAL", sfsys=false);
+        @test length(fs) == 33
+        res = main_looper(sort(fs; by = x->filesize(x.path))[8]);
+        @test bincounts(res[:SFnoZCutFlow]) == [0, 0, 0, 35, 19, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+        @test integral(res[:SFnoZ__BDT__NOMINAL]) ≈ 0.0005643258391566838
+    end
 end
